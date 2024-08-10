a4ff08c1169186c63c5a2063cab513e071d540fd
J3Sli3 <konjunder@gmail.com>
2024-08-10 17:19:47 -0400

    create Password_Generator.py
    Users can now create an efficient password using this password generator.

---
+++ Passsword_Generator.py
@@ -0,0 +1,33 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+rand_letters = random.choices(letters, k = int(nr_letters))
+
+rand_symbols = random.choices(symbols, k = int(nr_symbols))
+
+rand_numbers = random.choices(numbers, k = int(nr_numbers))
+
+password = rand_letters + rand_symbols + rand_numbers
+
+print(password)
+
+random.shuffle(password)
+
+print(password)
+
+encrypted_password = ""
+
+for i in password:
+    encrypted_password += i
+    
+print(f"Your password is: {encrypted_password} ")
+    
+
