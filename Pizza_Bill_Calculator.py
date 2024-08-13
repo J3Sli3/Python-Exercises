6bb1ddbd9f5b6fdc89d24e70a6435d8598e1a03f
J3Sli3 <konjunder@gmail.com>
2024-08-13 17:42:13 -0400

    Update Pizza_Bill_Calculator
    Incorporate smaller case input. Lazy people who don't want to press the higher case button to answer the questions can now answer in lower case.

--- Pizza_Bill_Calculator.py
+++ Pizza_Bill_Calculator.py
@@ -3,29 +3,30 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 cost = 0
 
-if size == "S":
+if size == "S" or size == 's':
     cost += 15
-elif size == "M":
+elif size == "M" or size == "m":
     cost += 20
-elif size == "L":
+elif size == "L" or size == "l":
     cost += 25
 else:
     print("You typed the wrong inputs.")
 
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-if pepperoni == "Y" and size == ("S"):
-    cost += 2
-elif pepperoni == "Y" and size == ("M" or "L"):
-    cost += 3
-elif pepperoni == "N":
+if pepperoni == "Y" or pepperoni == 'y':
+    if size == "S" or size == "s":
+        cost += 2
+    else:
+       cost += 3
+elif pepperoni == "N" or pepperoni == "n":
     cost += 0
 else:
     print("You typed the wrong inputs.")
 
 cheese = input("Do you want extra cheese? Y or N: ")
-if cheese == "Y":
+if cheese == "Y" or cheese == "y":
     cost += 1
-elif cheese == "N":
+elif cheese == "N" or cheese == "n":
     cost += 0
 
 print(f"Your final bill is: ${cost}.")
